cce8cfcafdec78abaa1cd8afc57e17a8a24da30c
slacker <slacker@slackers-MacBook-Pro.local>
2020-02-20 23:08:48 -0300

    Clean project

---
+++ iTunesFavoriteApp/app/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  iTunesFavoriteApp
+//
+//  Created by slacker on 20-02-20.
+//  Copyright © 2020 slacker. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
